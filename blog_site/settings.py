2b4fcbdfeeb937db9f9a39963bd9fa226f44d0dd
Yukino <85972420+ynagamine-dev@users.noreply.github.com>
2021-07-21 13:53:16 +0900

    Update settings.py

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -8,15 +8,13 @@ env = environ.Env()
 environ.Env().read_env()
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [env('ALLOWED_HOST1'), env('ALLOWED_HOST2'), env('ALLOWED_HOST3')]
+ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
 
 
 
@@ -121,12 +119,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-"""
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-"""
 
 
 STATIC_URL = '/static/'
